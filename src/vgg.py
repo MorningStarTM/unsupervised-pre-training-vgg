8ea0dba6282c9f600d13ada6c2f41c958968c071
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-14 22:29:51 +0530

    vgg decoder created

--- src/vgg.py
+++ src/vgg.py
@@ -36,3 +36,36 @@ def vgg_encoder(inputs:tuple):
     return encoder_model
 
 
+
+def vgg_decoder(inputs:tuple):
+    decoder_in = L.Input(shape=(8,8,256))
+
+    d = L.Conv2DTranspose(256, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(decoder_in)
+    d = L.Conv2DTranspose(256, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d)
+    d = L.Conv2DTranspose(256, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d)
+    d = L.UpSampling2D((2,2))(d)
+
+    d1 = L.Conv2DTranspose(128, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d)
+    d1 = L.Conv2DTranspose(128, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d1)
+    d1 = L.Conv2DTranspose(128, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d1)
+    d1 = L.UpSampling2D((2,2))(d1)
+
+    d2 = L.Conv2DTranspose(64, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d1)
+    d2 = L.Conv2DTranspose(64, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d2)
+    d2 = L.Conv2DTranspose(64, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d2)
+    d2 = L.UpSampling2D((2,2))(d2)
+
+    d3 = L.Conv2DTranspose(32, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d2)
+    d3 = L.Conv2DTranspose(32, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d3)
+    d3 = L.Conv2DTranspose(32, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d3)
+    d3 = L.UpSampling2D((2,2))(d3)
+
+    d4 = L.Conv2DTranspose(16, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d3)
+    d4 = L.Conv2DTranspose(16, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d4)
+    d4 = L.Conv2DTranspose(16, (6,6), activation='relu', padding='same', strides=(1,1), kernel_initializer='he_normal')(d4)
+    d4 = L.UpSampling2D((2,2))(d4)
+
+    outputs = L.Conv2DTranspose(3, (6,6), activation='sigmoid', padding='same', strides=(1,1), kernel_initializer='he_normal')(d4)
+
+    decoder_model = Model(decoder_in, outputs, name='decoder')
+
