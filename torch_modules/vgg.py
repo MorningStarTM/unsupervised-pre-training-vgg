e082cfb883c776b59cb2affbb798c8d7f8d7d4fd
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-15 09:10:23 +0530

    VGG decoder block added

--- torch_modules/vgg.py
+++ torch_modules/vgg.py
@@ -42,3 +42,26 @@ class VGGEncoder(nn.Module):
         x = self.block4(x)
         x = self.block5(x)
         return x
+    
+
+class VGGDecoder(nn.Module):
+    def __init__(self):
+        super(VGGDecoder, self).__init__()
+        self.upconv1 = self.upconv_block(256, 256)
+        self.upconv2 = self.upconv_block(256, 128)
+        self.upconv3 = self.upconv_block(128, 64)
+        self.upconv4 = self.upconv_block(64, 32)
+        self.upconv5 = self.upconv_block(32, 16)
+        self.final = nn.ConvTranspose2d(16, 3, kernel_size=6, stride=1, padding=2)
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.upconv1(x)
+        x = self.upconv2(x)
+        x = self.upconv3(x)
+        x = self.upconv4(x)
+        x = self.upconv5(x)
+        x = self.final(x)
+        x = self.activation(x)
+        return x
+
