67cbfbabcd5cce4759ec00e8e427b38a1da755a6
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-15 09:47:12 +0530

    updated

--- torch_modules/vgg.py
+++ torch_modules/vgg.py
@@ -2,13 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def conv_block(in_channels, out_channels, kernel_size=6, stride=1, padding=2):
+def conv_block(in_channels, out_channels, kernel_size=6, stride=1):
+    padding = (kernel_size - 1) // 2
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding="same"),
         nn.ReLU(inplace=True),
-        nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding="same"),
         nn.ReLU(inplace=True),
-        nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding="same"),
         nn.ReLU(inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2)
     )
@@ -16,11 +17,11 @@ def conv_block(in_channels, out_channels, kernel_size=6, stride=1, padding=2):
 
 def upconv_block(self, in_channels, out_channels, kernel_size=6, stride=1, padding=2):
     return nn.Sequential(
-        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding="same"),
         nn.ReLU(inplace=True),
-        nn.ConvTranspose2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.ConvTranspose2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding="same"),
         nn.ReLU(inplace=True),
-        nn.ConvTranspose2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+        nn.ConvTranspose2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding="same"),
         nn.ReLU(inplace=True),
         nn.Upsample(scale_factor=2, mode='nearest')
     )
@@ -47,11 +48,11 @@ class VGGEncoder(nn.Module):
 class VGGDecoder(nn.Module):
     def __init__(self):
         super(VGGDecoder, self).__init__()
-        self.upconv1 = self.upconv_block(256, 256)
-        self.upconv2 = self.upconv_block(256, 128)
-        self.upconv3 = self.upconv_block(128, 64)
-        self.upconv4 = self.upconv_block(64, 32)
-        self.upconv5 = self.upconv_block(32, 16)
+        self.upconv1 = upconv_block(256, 256)
+        self.upconv2 = upconv_block(256, 128)
+        self.upconv3 = upconv_block(128, 64)
+        self.upconv4 = upconv_block(64, 32)
+        self.upconv5 = upconv_block(32, 16)
         self.final = nn.ConvTranspose2d(16, 3, kernel_size=6, stride=1, padding=2)
         self.activation = nn.Sigmoid()
 
